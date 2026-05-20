b65c774282eee0c3061b9d845d4ca66ef10c6660
gravi-ctrl <gravi-ctrl@noreply.codeberg.org>
2026-05-20 13:29:17 +0200

    Update run_once/setup.sh

--- run_once/setup.sh
+++ run_once/setup.sh
@@ -153,13 +153,12 @@ fi
 header "4/10" "Directory Structure & Permissions"
 
 task "Create /data directory tree"
-sudo mkdir -p /data/borg_backup
 sudo mkdir -p /data/paperless
 sudo mkdir -p /data/assets/torrents
-sudo mkdir -p /data/assets/Media/{Movies,Shows,Music,Books,Podcasts}
 sudo mkdir -p /data/assets/downloads
-sudo mkdir -p /data/assets/romm/{library,resources}
 sudo mkdir -p /data/assets/nextcloud_data
+sudo mkdir -p /data/assets/romm/{library,resources}
+sudo mkdir -p /data/assets/Media/{Movies,Shows,Music,Books,Podcasts}
 sudo mkdir -p /data/assets/syncthing/{Apps,Backup,DCIM,Movies,Music,My_Shit,Shared}
 pass
 
