2e6638bf4901f52a0167fb4e29da99174badd766
gravi-ctrl <12@34567.8shield.net>
2026-05-26 00:14:25 +0300

    Explanation for why regctl is even needed

--- run_once/setup.sh
+++ run_once/setup.sh
@@ -101,7 +101,7 @@ else
     pass "already installed"
 fi
 
-task "Install regctl"
+task "Install regctl (for Dockcheck)"
 if ! command -v regctl &> /dev/null; then
     quietly sudo curl -fsSL https://github.com/regclient/regclient/releases/latest/download/regctl-linux-amd64 -o /usr/local/bin/regctl
     sudo chmod +x /usr/local/bin/regctl
