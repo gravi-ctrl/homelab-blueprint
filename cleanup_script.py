a269d0f9224493281c5f07e54a5016d68c566a3b
gravi-ctrl <gravi-ctrl@noreply.codeberg.org>
2026-06-07 22:26:46 +0200

    Update cleanup_script.py

--- cleanup_script.py
+++ cleanup_script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # @DESCRIPTION: Rotates backups by keeping the N most recent items or purging folders entirely. Includes Dry Run safety mode.
 # @FREQUENCY: Daily 1am and 1pm
+# @USES_ENV: FILES_TO_KEEP
 #
 # --- USAGE EXAMPLES ---
 # 1. Standard (Keep 2 most recent):
